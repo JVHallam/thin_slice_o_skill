efeb11b3d88810cf8dffaf95f4f3753ee9757fa3
jake <jakehallam95@gmail.com>
2021-08-11 13:00:57 +0100

    Added context stuff

--- spike/entity_test/Program.cs
+++ spike/entity_test/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace entity_test
 {
+
+    public class CustomerContext : DbContext{
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options){
+            var connectionString = Environment.GetEnvironmentVariable("connection_string");
+            options.UseSqlServer(connectionString);
+        }
+    }
+
+    public class Customer{
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var a = Environment.GetEnvironmentVariable("connection_string");
             Console.WriteLine("Hello World!");
+            Console.WriteLine(a);
         }
     }
 }
